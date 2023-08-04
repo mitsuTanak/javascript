1fa1d91c65e03893349f3c6935f210906c9e5253
mitsuTanak <130664643+mitsuTanak@users.noreply.github.com>
2023-08-04 15:13:00 -0300

    mjynhtgbrfe

---
+++ exemplos/2_tipos de dados/2_operações_aritimeticas/script.js
@@ -0,0 +1,10 @@
+console.log((2+3));
+console.log((5-12));
+console.log((4*4));
+console.log((5/3));
+
+// O operador % chama-se MOD
+// e Exime o resto de uma divisão
+
+console.log((20 % 2))
+console.log((21 % 2))
---
+++ exemplos/2_tipos de dados/4_string/script.js
@@ -0,0 +1,10 @@
+console.log("Meu nome é Pedro");
+console.log('desenvolvimento é no Senai');
+
+// Templaite literals (crase) ao lado da tecla "P"
+// Usar shift
+
+console.log(`teste`);
+
+console.log(typeof 'Infinity');
+console.log(typeof Infinity);
