0297ed8af4614869f53dd5f6f0e0486e0f378529
mitsuTanak <130664643+mitsuTanak@users.noreply.github.com>
2023-11-16 08:51:32 -0300

    aaa

--- exemplos/08-Dom/dom.js
+++ exemplos/08-Dom/dom.js
@@ -2,14 +2,14 @@
 
 
 // Encontre o elemento <img> pelo atributo alt
-const imgElement = document.querySelector('img[alt="DOM"]');
+// const imgElement = document.querySelector('img[alt="DOM"]');
 
 // Defina o caminho local da imagem
 // const caminhoLocal = 'img/A_Veloz.jpg';
-const caminhoLocal = 'img/B_Veloz.jpg';
+// const caminhoLocal = 'img/B_Veloz.jpg';
 
 // Altere o atributo src do elemento <img> para o caminho local
-imgElement.setAttribute('src', caminhoLocal);
+// imgElement.setAttribute('src', caminhoLocal);
 
 // ____________________________________________________________________________
 
@@ -24,6 +24,16 @@ querySelectorAll() -> seleciona VÁRIOS elementos de acordo com um seletor */
 
 
 // 1ª Digitação (Aqui)
+// Selecionando pelo Id
+const titulo = document.getElementById('titulo-principal');
+
+// Selacionado pela TAG
+const pagina = document.querySelector('body');
+const legenda = document.querySelector('figcaption');
+
+// Selecionar todas as TAG's 'H2'
+const subtitulo = document.querySelectorAll('h2');
+const paragrafo = document.querySelectorAll('p');
 
 
 // ____________________________________________________________________________
@@ -32,6 +42,20 @@ querySelectorAll() -> seleciona VÁRIOS elementos de acordo com um seletor */
 
 
 // 2ª Digitação (Aqui)
+// Mudando o primeiro H1
+titulo.textContent = "Hello World";
+
+// Cor do Fundo (backgroundColor)
+pagina.style.backgroundColor = "lightgreen";
+
+// Mudando o Segundo H2
+subtitulo[1].textContent = 'Full-Stack';
+
+
+// Mudando a Legenda (figcaption)
+legenda.innerHTML = "<b> Veloz e Furioso </b>"
+
+
 
 
 // ____________________________________________________________________________
@@ -54,9 +78,9 @@ const subtitulos = document.querySelectorAll('h2');
 // });
 
 // Sintaxe For...of (Loop comum)
-// for (const subtitulo of subtitulos) {
-//     subtitulo.style.color = 'blue';
-//   }
+for (const subtitulo of subtitulos) {
+    subtitulo.style.color = 'blue';
+  }
 
 
 // ____________________________________________________________________________
@@ -67,13 +91,23 @@ const subtitulos = document.querySelectorAll('h2');
 
 
 // 3ª Digitação (Aqui)
+// Modificar Texto (H3)
+const tituloEditores = document.querySelector('h3');
 
+tituloEditores.textContent = "Melhores Editores";
+tituloEditores.style.color = "orange";
+tituloEditores.style.fontSize = "28px";
 
-// Adicionar um novo editor à lista:
 
 
 // 4ª Digitação (Aqui)
 
+// Adicionar um novo editor à lista:
+const listaEditores = document.querySelector('#lista-editores');
+const novoEditor = document.createElement('li');
 
+novoEditor.textContent = 'Sublime';
+listaEditores.appendChild(novoEditor);
 
-
+const terceiroITEM = listaEditores.querySelectorAll('li')[2];
+terceiroITEM.style.color = 'orange';
